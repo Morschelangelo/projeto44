329b11aad5fe740c8daa1f56cfd4a0c5548ce64f
Vincent <164825800+Morschelangelo@users.noreply.github.com>
2025-02-17 15:09:30 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,35 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require ('sass'))
-const imagemin = require('gulp-imagemin')
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
+// Process JavaScript files and output to public/js
 function scripts() {
-    return gulp.src('./src/scripts/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/js'))
+    return gulp.src('./src/scripts/*.js') // Source folder for JS files
+        .pipe(uglify()) // Minify JavaScript
+        .pipe(gulp.dest('./public/js')); // Output to public/js
 }
 
+// Compile SCSS to CSS and output to public/css
 function styles() {
-    return gulp.src('./src/styles/*.scss')
-        .pipe(sass({outputStyle: 'compressed' }))
-        .pipe(gulp.dest('./dist/css'));
+    return gulp.src('./src/styles/*.scss') // Source folder for SCSS files
+        .pipe(sass({ outputStyle: 'compressed' })) // Compile and compress the CSS
+        .pipe(gulp.dest('./public/css')); // Output to public/css
 }
 
+// Optimize images and output to public/images
 function images() {
-    return gulp.src('./src/images/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'));
+    return gulp.src('./src/images/**/*') // Source folder for images
+        .pipe(imagemin()) // Optimize images
+        .pipe(gulp.dest('./public/images')); // Output to public/images
 }
 
+// Define the default task to run all tasks in parallel
 exports.default = gulp.parallel(styles, images, scripts);
 
+// Watch files for changes
 exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
-}
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles)); // Watch for SCSS changes
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts)); // Watch for JS changes
+    gulp.watch('./src/images/**/*', gulp.parallel(images)); // Watch for image changes
+};
